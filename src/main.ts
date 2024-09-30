7997a11598e58ac12041f88cdf4017bbef36de95
GustavoJVvieira <gustavojvieira2004@gmail.com>
2024-09-29 23:00:01 -0300

    Documentando com Swagger

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('Documentação API ')
+  .setDescription('API para projeto de Lembretes desenvolvido para Processo Seletivo dti')
   .setVersion('1.0')
-  .addTag('cats')
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
